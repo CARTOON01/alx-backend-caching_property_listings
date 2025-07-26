0d4d48b750af55a81eafe409180aeeb4cdc4355e
Brian Kamau <brian@briankamau.com>
2025-07-26 22:54:52 +0300

    modify total to include total requests

--- properties/utils.py
+++ properties/utils.py
@@ -22,8 +22,8 @@ def get_redis_cache_metrics():
         info = conn.info()
         hits = info.get("keyspace_hits", 0)
         misses = info.get("keyspace_misses", 0)
-        total = hits + misses
-        hit_ratio = (hits / total) if total > 0 else 0
+        total_requests = hits + misses
+        hit_ratio = (hits / total_requests) if total_requests > 0 else 0
 
         metrics = {
             "keyspace_hits": hits,
