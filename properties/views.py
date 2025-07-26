af4694186597432d16adb85e9bf17285aa85344f
Brian Kamau <brian@briankamau.com>
2025-07-26 22:45:40 +0300

    queryset

--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .models import Property
+from .utils import get_all_properties
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.all()
+    properties = get_all_properties()
     if request.GET.get("format") == "json":
         data = [
             {
